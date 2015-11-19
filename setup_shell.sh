25e30ab8a5dc2ce75a5fa431404e5457e2f52008
Jake Larkin <jake8@uber.com>
2015-11-18 16:07:52 -0800

    Create setup_shell.sh

---
+++ setup_shell.sh
@@ -0,0 +1,47 @@
+
+# BEGIN OSX SPECIFIC
+
+# install command line tools w/o xcode
+xcode-select --install
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install coreutils
+brew install zsh
+brew install git
+
+# install a 'nice' version of vim
+brew install vim --override-system vim
+
+# END OSX Specific
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# install my custom prompt theme
+curl -fsSl https://raw.githubusercontent.com/jake8larkin/dotfiles/master/.oh-my-zsh/themes/jakeo.zsh-theme > ~/.oh-my-zsh/themes/jakeo.zsh-theme
+sed -i '' 's/ZSH_THEME\=\".*"/ZSH_THEME="jakeo"/' ~/.zshrc
+
+# symlink vi to vim
+ln -s /usr/local/bin/vim /usr/local/bin/vi
+sudo mv /usr/bin/vi /usr/bin/vi_old
+
+# get my .vimrc
+curl -fsSSL https://raw.githubusercontent.com/jake8larkin/dotfiles/master/.vimrc > ~/.vimrc
+
+# install pathogen vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# install custim vim color theme (sublime inspired)
+mkdir -p ~/.vim/colors
+curl -fsSSL https://raw.githubusercontent.com/jake8larkin/dotfiles/master/.vim/colors/molokai.vim > ~/.vim/colors/molokai.vim
+
+mkdir -p ~/.vim/bundle
+git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+git clone https://github.com/moll/vim-node.git ~/.vim/bundle/node
+git clone https://github.com/scrooloose/syntastic.git ~/.vim/bundle/syntastic
+git clone https://github.com/Raimondi/delimitMate ~/.vim/bundle/delimitMate
+git clone https://github.com/pangloss/vim-javascript.git ~/.vim/bundle/vim-javascript
+git clone https://github.com/derekwyatt/vim-scala ~/.vim/bundle/vim-scala
+#git clone https://github.com/kchmck/vim-coffee-script.git ~/.vim/bundle/vim-coffee-script/ #yuck
+
+curl -fsSSL https://raw.githubusercontent.com/jake8larkin/dotfiles/master/.gitignore_global >> ~/.gitignore_global
